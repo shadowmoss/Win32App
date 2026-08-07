665b15a03004bf2e8ba048b06c55d5b92cc0cf76
shadowmoss <18284806010@163.com>
2026-08-07 17:41:38 +0800

    1、Win32Accessroter Table

--- directX2d_mouse_draw.cpp
+++ directX2d_mouse_draw.cpp
@@ -1,9 +1,28 @@
+#include <windowsx.h>
 #include <windows.h>
 #include <d2d1.h>
 #pragma comment(lib, "d2d1")
 
 #include "basewin.h"
+class DPIScale
+{
+    static float scale;
 
+public:
+    static void Initialize(HWND hwnd)
+    {
+        float dpi = GetDpiForWindow(hwnd);
+        scale = dpi/96.0f;
+    }
+
+    template <typename T>
+    static D2D1_POINT_2F PixelsToDips(T x, T y)
+    {
+        return D2D1::Point2F(static_cast<float>(x) / scale, static_cast<float>(y) / scale);
+    }
+};
+
+float DPIScale::scale = 1.0f;
 template <class T> void SafeRelease(T **ppT)
 {
     if (*ppT)
@@ -19,6 +38,7 @@ class MainWindow : public BaseWindow<MainWindow>
     ID2D1HwndRenderTarget   *pRenderTarget;
     ID2D1SolidColorBrush    *pBrush;
     D2D1_ELLIPSE            ellipse;
+    D2D1_POINT_2F           ptMouse;
 
     void    CalculateLayout();
     HRESULT CreateGraphicsResources();
@@ -28,26 +48,31 @@ class MainWindow : public BaseWindow<MainWindow>
 
 public:
 
-    MainWindow() : pFactory(NULL), pRenderTarget(NULL), pBrush(NULL)
+    MainWindow() : pFactory(NULL), pRenderTarget(NULL), pBrush(NULL),ellipse(D2D1::Ellipse(D2D1::Point2F(),0,0)),ptMouse(D2D1::Point2F())
     {
     }
 
     PCWSTR  ClassName() const { return L"Circle Window Class"; }
     LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+    void OnLButtonDown(int pixelX,int pixelY,DWORD flags);
+    void OnLButtonUp();
+    void OnMouseMove(int pixelX,int pixelY,DWORD flags); 
 };
 
 // Recalculate drawing layout when the size of the window changes.
 
 void MainWindow::CalculateLayout()
 {
-    if (pRenderTarget != NULL)
-    {
-        D2D1_SIZE_F size = pRenderTarget->GetSize();
-        const float x = size.width / 2;
-        const float y = size.height / 2;
-        const float radius = min(x, y);
-        ellipse = D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius);
-    }
+    // if (pRenderTarget != NULL)
+    // {
+    //     D2D1_SIZE_F size = pRenderTarget->GetSize();
+    //     const float x = size.width / 2;
+    //     const float y = size.height / 2;
+    //     const float radius = min(x, y);
+    //     ellipse = D2D1::Ellipse(D2D1::Point2F(x, y), radius, radius);
+    // }
+
 }
 
 HRESULT MainWindow::CreateGraphicsResources()
@@ -127,6 +152,32 @@ void MainWindow::Resize()
     }
 }
 
+void MainWindow::OnLButtonDown(int pixelX,int pixelY,DWORD flags){
+    SetCapture(m_hwnd);
+    ellipse.point = ptMouse = DPIScale::PixelsToDips(pixelX,pixelY);
+    ellipse.radiusX = ellipse.radiusY = 1.0f;
+    InvalidateRect(m_hwnd,NULL,FALSE);
+}
+
+void MainWindow::OnMouseMove(int pixelX,int pixelY,DWORD flags){
+    if(flags & MK_LBUTTON){
+        const D2D1_POINT_2F dips = DPIScale::PixelsToDips(pixelX,pixelY);
+
+        const float width = (dips.x - ptMouse.x)/2;
+        const float height = (dips.y - ptMouse.y)/2;
+        const float x1 = ptMouse.x + width;
+        const float y1 = ptMouse.y + height;
+
+        ellipse = D2D1::Ellipse(D2D1::Point2F(x1,y1),width,height);
+
+        InvalidateRect(m_hwnd,NULL,FALSE);
+    }
+}
+
+void MainWindow::OnLButtonUp(){
+    ReleaseCapture();
+}
+
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR, int nCmdShow)
 {
     MainWindow win;
@@ -162,6 +213,7 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
         {
             return -1;  // Fail CreateWindowEx.
         }
+        DPIScale::Initialize(m_hwnd);
         return 0;
 
     case WM_DESTROY:
@@ -172,14 +224,22 @@ LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
         return 0;
 
     case WM_PAINT:
-        
         OnPaint();
         return 0;
 
-
-
     case WM_SIZE:
         Resize();
+        return 0;
+    
+    case WM_LBUTTONDOWN:
+        OnLButtonDown(GET_X_LPARAM(lParam),GET_Y_LPARAM(lParam),(DWORD)wParam);
+        return 0;
+    
+    case WM_LBUTTONUP:
+        OnLButtonUp();
+        return 0;
+    case WM_MOUSEMOVE:
+        OnMouseMove(GET_X_LPARAM(lParam),GET_Y_LPARAM(lParam),(DWORD)wParam);
         return 0;
     }
     return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
